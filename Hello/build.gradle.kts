aca5f803b40da44f9bacb927cd87bf7b8bacff0e
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-04-13 17:23:51 +0900

    Chore: 추가 - ktlint gradle, .editorconfig

--- Hello/build.gradle.kts
+++ Hello/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application") version "7.4.1" apply false
     id("com.android.library") version "7.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.3.1"
 }
 
 tasks.register("clean", Delete::class) {
