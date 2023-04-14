ae886510b312398a3c5d18534ab5070817f5c18f
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-04-14 12:33:55 +0900

    Chore: 생성 - :core:designsystem

---
+++ Hello/core/designsystem/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    kotlin("android")
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.purple.core.designsystem"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = KotlinOptions.jvmTarget
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = ComposeOptions.kotlinCompilerExtensionVersion
+    }
+}
+
+dependencies {
+    implementation(composeDependencies)
+    debugImplementation(composeDebug)
+
+    implementation(androidLibraryDependencies)
+    testImplementation(defaultUnitTest)
+    androidTestImplementation(defaultAndroidTest)
+}
---
+++ Hello/core/designsystem/src/main/java/com/purple/core/designsystem/TEST.kt
@@ -0,0 +1,11 @@
+package com.purple.core.designsystem
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun Test() {
+    Text(text = "abc")
+}
