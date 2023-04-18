b1d324530c0444cf060e1bc877d1bb3823dd1efe
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-04-18 15:04:01 +0900

    Feat: 추가 - :domain:rooms model - Room, User, Feed [S08P31A408-171]

--- Hello/domain/rooms/src/main/java/com/purple/hello/domain/feature/model/Room.kt
+++ Hello/domain/rooms/src/main/java/com/purple/hello/domain/feature/model/Room.kt
@@ -2,18 +2,10 @@ package com.purple.hello.domain.feature.model
 
 data class Room(
     val id: Int,
-    val option: RoomOption,
+    val name: String,
     val members: List<User>,
 )
 
-data class RoomOption(
-    val roomName: String,
+data class AdminRoomOptions(
     val passwordQuestion: String,
-    val password: String,
-)
-
-data class User(
-    val id: Int,
-    val name: String,
-    val profileUrl: String,
 )
