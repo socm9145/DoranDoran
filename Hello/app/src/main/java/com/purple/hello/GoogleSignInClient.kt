6248bec8bca3608cec1b90fcbe0f10fad5ae2c66
phang <leeckdwns9305@gmail.com>
2023-04-24 17:57:37 +0900

    Feat: [:app] login - Google login start & signinbutton :core 로 이동 [S08P31A408-180

---
+++ Hello/app/src/main/java/com/purple/hello/GoogleSignInClient.kt
@@ -0,0 +1,12 @@
+package com.purple.hello
+
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+fun getGoogleSignInClient(context: Context): GoogleSignInClient {
+    val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        .build()
+    return GoogleSignIn.getClient(context, signInOptions)
+}
--- Hello/app/src/main/java/com/purple/hello/LoginScreen.kt
+++ Hello/app/src/main/java/com/purple/hello/LoginScreen.kt
@@ -1,25 +1,16 @@
 package com.purple.hello
 
 import android.app.Application
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.purple.core.designsystem.component.SignInButton
 import com.purple.core.designsystem.theme.HiTheme
 
 @Composable
@@ -84,53 +75,6 @@ private fun SignInGoogleButton() {
         painter = painterResource(id = R.drawable.ic_google_logo),
         contentText = "구글 로그인",
     )
-}
-
-@Composable
-private fun SignInButton(
-    onClick: () -> Unit,
-    buttonContentColor: Color,
-    painter: Painter,
-    contentText: String,
-) {
-    HiTheme {
-        Surface(
-            modifier = Modifier
-                .clickable { onClick() },
-            border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.surface),
-            color = buttonContentColor,
-            shape = RoundedCornerShape(6.dp),
-        ) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .height(40.dp)
-                    .padding(vertical = 8.dp, horizontal = 10.dp)
-                    .animateContentSize(
-                        animationSpec = tween(
-                            durationMillis = 300,
-                            easing = LinearOutSlowInEasing,
-                        ),
-                    ),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-            ) {
-                Image(
-                    modifier = Modifier
-                        .size(18.dp),
-                    painter = painter,
-                    contentDescription = contentText,
-                )
-                Spacer(modifier = Modifier.width(24.dp))
-                Text(
-                    text = contentText,
-                    color = MaterialTheme.colorScheme.outline,
-                    fontSize = 14.sp,
-                )
-                Spacer(modifier = Modifier.width(24.dp))
-            }
-        }
-    }
 }
 
 @Preview
