f4f8e360cacbe930abf879af5767b97d34beb726
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-04-25 15:47:49 +0900

    Feat: 작성중 - remote DTO [S08P31A408-251]

---
+++ Hello/data/rooms/src/main/java/com/purple/data/rooms/RoomDataSource.kt
@@ -0,0 +1,4 @@
+package com.purple.data.rooms
+
+interface RoomDataSource {
+}
---
+++ Hello/data/rooms/src/main/java/com/purple/data/rooms/remote/UserDTO.kt
@@ -0,0 +1,10 @@
+package com.purple.data.rooms.remote
+
+import kotlinx.serialization.Serializable
+
+//@Serializable
+//data class UserDTO(
+//    val id: Long,
+//    val name: String,
+//    val profile: String
+//)
