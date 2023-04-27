7455fef37fa14b28a221f449e1cb7b358567b184
rhdmsqls5 <rhdmsqls5@gmail.com>
2023-04-27 18:02:29 +0900

    Feat: 수정 - core:database to android module

--- Hello/core/database/build.gradle.kts
+++ Hello/core/database/build.gradle.kts
@@ -1,12 +1,20 @@
 plugins {
-    id("java-library")
+    id("com.android.library")
+    id("com.google.dagger.hilt.android")
+    kotlin("android")
     kotlin("kapt")
-    kotlin("jvm")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+android {
+    namespace = "com.purple.core.database"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
 }
 
 dependencies {
@@ -14,7 +22,9 @@ dependencies {
     implementation(Room.ROOM_KTX)
     implementation(Room.ROOM_COMMON)
     implementation(Kotlin.KOTLIN_COROUTINES)
-
     annotationProcessor(Room.ROOM_COMPILER)
     kapt(Room.ROOM_COMPILER)
+
+    implementation(Hilt.HILT_ANDROID)
+    kapt(Hilt.HILT_ANDROID_COMPILER)
 }
