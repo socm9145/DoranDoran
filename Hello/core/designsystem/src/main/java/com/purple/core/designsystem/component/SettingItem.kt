2d5e990dd2e7e64e501445474dd81d44b1657f42
phang <leeckdwns9305@gmail.com>
2023-05-02 10:23:07 +0900

    Layout: [:core:designsystem] ssettingitem 작성 [S08P31A408-280]

--- Hello/core/designsystem/src/main/java/com/purple/core/designsystem/component/SettingItem.kt
+++ Hello/core/designsystem/src/main/java/com/purple/core/designsystem/component/SettingItem.kt
@@ -17,13 +17,38 @@ import com.purple.core.designsystem.icon.HiIcons
 import com.purple.core.designsystem.theme.HiTheme
 import com.purple.core.designsystem.theme.HiTypography
 
+enum class TextType(val mainText: String, val subText: String, val isWarning: Boolean, val isButtonIcon: Boolean) {
+    CHANGE_ROOM_NAME("그룹 이름 변경", "나에게 보이는 그룹 이름을 변경할 수 있어요", false, true),
+    CHANGE_NAME("나의 이름 변경", "그룹에서 사용할 나의 이름을 변경할 수 있어요", false, true),
+    CHANGE_PASSWORD("그룹 비밀번호 변경", "그룹 관리자에게만 가능한 기능이에요", false, true),
+    EXIT_GROUP("그룹에서 나가기", "나가면 다시 초대를 받아야 들어올 수 있어요", true, false),
+    DELETE_GROUP("그룹 삭제", "그룹을 삭제하면 아무것도 남지않아요....ㅠ", true, false),
+    POLICY("개인 정보 처리 방침", "", false, true),
+    APP_INFO("시스템(앱) 정보", "", false, true),
+    LOGOUT("로그아웃", "다시 로그인하면 모든 정보를 다시 보실 수 있어요", true, false),
+    DELETE_USER("회원 탈퇴", "탈퇴하시면 지금까지의 모든 정보가 사라져요!", true, false),
+}
+
+data class TextData(
+    val mainText: String,
+    val subText: String,
+    val isWarning: Boolean,
+    val isButtonIcon: Boolean,
+)
+
+fun createTextType(type: TextType): TextData {
+    return TextData(
+        mainText = type.mainText,
+        subText = type.subText,
+        isWarning = type.isWarning,
+        isButtonIcon = type.isButtonIcon,
+    )
+}
+
 @Composable
 fun SettingItem(
     onClick: () -> Unit,
-    isButtonIcon: Boolean,
-    mainText: String,
-    isWarning: Boolean,
-    subText: String,
+    content: TextData,
 ) {
     HiTheme {
         Box(
@@ -40,8 +65,8 @@ fun SettingItem(
                 Arrangement.SpaceBetween,
                 Alignment.CenterVertically,
             ) {
-                SettingItemContent(mainText, isWarning, subText)
-                if (isButtonIcon) {
+                SettingItemContent(content)
+                if (content.isButtonIcon) {
                     Icon(
                         imageVector = HiIcons.ArrowRight,
                         contentDescription = "더 보기",
@@ -54,9 +79,7 @@ fun SettingItem(
 
 @Composable
 private fun SettingItemContent(
-    mainText: String,
-    isWarning: Boolean,
-    subText: String,
+    content: TextData,
 ) {
     Column(
         modifier = Modifier
@@ -65,14 +88,14 @@ private fun SettingItemContent(
     ) {
         Box(contentAlignment = Alignment.CenterStart) {
             Text(
-                text = mainText,
-                color = if (isWarning) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.outline,
+                text = content.mainText,
+                color = if (content.isWarning) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.outline,
                 style = HiTypography.headlineSmall,
             )
         }
         Box(contentAlignment = Alignment.CenterStart) {
             Text(
-                text = subText,
+                text = content.subText,
                 color = MaterialTheme.colorScheme.onSurfaceVariant,
                 style = HiTypography.bodyLarge,
             )
@@ -87,10 +110,7 @@ private fun PreviewSettingItem() {
         Surface(color = MaterialTheme.colorScheme.background) {
             SettingItem(
                 onClick = {},
-                mainText = "그룹 이름 변경",
-                isWarning = false,
-                subText = "나에게 보이는 그룹 이름을 변경할 수 있어요",
-                isButtonIcon = true,
+                createTextType(TextType.CHANGE_ROOM_NAME),
             )
         }
     }
