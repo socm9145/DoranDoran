7a2b87ee629e484f5628d3254bbad484c2a95d95
김태현 <socm9145@naver.com>
2023-05-02 16:55:38 +0900

    backend_swagger: SwaggerConfiugration에 server 추가

--- backend/hello/src/main/java/com/purple/hello/config/SwaggerConfiguration.java
+++ backend/hello/src/main/java/com/purple/hello/config/SwaggerConfiguration.java
@@ -12,6 +12,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -20,7 +21,10 @@ public class SwaggerConfiguration {
     private static final String REFERENCE  = "Bearer ";
     @Bean
     public Docket api() {
+        Server localServer = new Server("local", "http://localhost:8000", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server testServer = new Server("test", "https://www.doeran.kr/api", "for testing", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.OAS_30)
+                .servers(localServer, testServer)
                 .useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드 (200, 401, 403, 404) 등의 노출 여부
                 .apiInfo(apiInfo()) // Swagger UI 로 노출할 정보
                 .select()
